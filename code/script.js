a8e3223e0aad271431c29ec9b52f6f2a4fa09c10
Ken-Sama-tech <dikoalambastaacc@gmail.com>
2024-11-23 11:38:40 +0800

    added some html
    idk

--- code/script.js
+++ code/script.js
@@ -4,12 +4,42 @@ document.addEventListener('DOMContentLoaded', function(){
     navbarLinks.forEach(link =>{
         if(link.href.includes(window.location.pathname)){
             link.classList.add('active');
-            let docTitle = link.textContent;
-            docTitle = docTitle
+            let Title = link.textContent;
+            Title = Title
                 .split(' ') 
                 .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) 
                 .join(' ');
-            document.title = docTitle; 
+            document.title = Title; 
         }
     });
+    //dasboard functions
+     const gradeLevel = document.querySelectorAll('.grade__level--box');
+
+     gradeLevel.forEach(level =>{
+        level.addEventListener('click', ()=>{
+            gradeLevel.forEach(g => g.classList.remove('active'));
+            level.classList.add('active');
+
+            const create = document.createElement('table');
+
+            create.setAttribute('class', 'dashboard-table');
+
+            const tableSection = document.querySelector('.table__section');
+
+            tableSection.appendChild(create);
+
+            const createTr = document.createElement('tr');
+
+            create.appendChild(createTr);
+
+            for(let i = 0; i < 4; i++){
+                const createTh = document.createElement('th');
+
+                createTh.setAttribute('class', 'table-heading');
+                create.textContent = 'diko alam';
+                createTr.appendChild(createTh);
+            }
+        });
+     });
 });
+
